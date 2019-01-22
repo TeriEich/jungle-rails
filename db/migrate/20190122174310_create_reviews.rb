12f37b9c8e177bec9ed3152ccf403d9d0a22fa97
Teri <teri.eich@gmail.com>
2019-01-22 19:56:57 +0000

    added review model

---
+++ db/migrate/20190122174310_create_reviews.rb
@@ -0,0 +1,17 @@
+class CreateReviews < ActiveRecord::Migration
+  def change
+    create_table :reviews do |t|
+      t.integer :product_id
+      t.integer :user_id
+      t.text :description
+      t.integer :rating
+      t.datetime :created_at
+      t.datetime :updated_at
+
+      t.timestamps null: false
+    end
+
+    add_foreign_key "reviews", "products"
+
+  end
+end
